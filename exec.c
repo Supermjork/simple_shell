96f713fd438447baaed545eb55f81ca6600cde31
linlyboi <linlysmolworthy@gmail.com>
2023-09-19 22:12:56 +0300

    exec_child added

---
+++ exec.c
@@ -0,0 +1,19 @@
+#include "shell.h"
+
+/**
+ * exec_child: exec this
+ * @args: le arguments
+ * @env: environment
+ * Return: exit status of child
+ */
+int exec_child(char **args, char **env)
+{
+	pid_t child;
+
+	child = fork();
+	if (child == 0)
+	{
+		if (execve(*args, args, NULL) == -1)
+			perror("hsh");
+	}
+}
